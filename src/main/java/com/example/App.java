8f47c79c32a2025efc8c618f68429116ddcbb80e
lai2-01 <lai2-01@itismeucci.com>
2023-10-03 09:15:21 +0200

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try{
+            Socket s = new Socket("localhost", 3000);
+            
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+            out.writeBytes("ciao a tutti" + "\n");
+            String risposta = in.readLine();
+
+            System.out.println("Il server ha detto " + risposta);
+
+            s.close();
+
+        } catch ( Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Qualcosa e' andato storto");
+            System.exit(1);
+        }
+    }
+}
